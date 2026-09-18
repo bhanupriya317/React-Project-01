a4cc38e5ff0b65846503e980ac7392192ff00a21
bhanu <bhanupriya.v2006@gmail.com>
2026-09-18 12:36:45 +0530

    Updated

---
+++ react-demo/src/components/Hero.jsx
@@ -0,0 +1,14 @@
+function Hero(){
+    return(
+        <section className="hero">
+            <p className="hero-label">College Events and Clubs</p>
+            <h1>Discover what is happening?</h1>
+            <p className="hero-description">
+                Find Workshops,sports activities,club metting
+            </p>
+            <a className="hero-button" href="#events">Explopre Events</a>
+        </section>
+
+    );
+}
+export default Hero;
