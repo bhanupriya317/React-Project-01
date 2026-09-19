1e7aacbeb88a6b0ffc44dc17f948189c7d2ef9d1
bhanu <bhanupriya.v2006@gmail.com>
2026-09-19 16:11:41 +0530

    UPDATE

--- react-demo/src/components/Navbar.jsx
+++ react-demo/src/components/Navbar.jsx
@@ -14,15 +14,31 @@
 // }
 // export default Navbar;
 
+import { NavLink } from "react-router";
+
 function Navbar() {
+  function getNavLinkClass({ isActive }) {
+    return isActive ? "nav-link active-link" : "nav-link";
+  }
+
   return (
     <nav className="navbar">
-      <h2>Campus Connect</h2>
+      <NavLink className="brand-name" to="/">
+        Campus Connect
+      </NavLink>
 
       <div className="nav-links">
-        <a href="#home">Home</a>
-        <a href="#events">Events</a>
-        <a href="#about">About</a>
+        <NavLink className={getNavLinkClass} to="/">
+          Home
+        </NavLink>
+
+        <NavLink className={getNavLinkClass} to="/events">
+          Events
+        </NavLink>
+
+        <NavLink className={getNavLinkClass} to="/about">
+          About
+        </NavLink>
       </div>
     </nav>
   );
